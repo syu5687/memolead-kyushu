36e25e308c15b7ee8a68c9801e846bee4e7d5448
01emastaff <matsufuji@emanet.jp>
2026-07-07 14:11:06 +0900

    店舗情報追加、情報一本化

--- src/data/site.ts
+++ src/data/site.ts
@@ -4,6 +4,10 @@ export const SITE = {
 	sub: 'エンゼルセンター',
   },
   fullName: 'エンゼルセンター仏光堂',
+  hours: '9:00-17:00',
+  closed: '',
+  area: '',
+
 } as const;
 
 
@@ -14,18 +18,24 @@ export const COMPANIES = {
 	zip: '850-0077',
 	address: '長崎県長崎市小瀬戸町809-12',
 	tel: '095-865-4400',
+    fax: '--',
 	hours: '9:00-17:00',
-	closed: '',
-	area: '',
+    email: '--',  
+	closed: '--',
+	area: '--',
+    mapEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3356.332817197784!2d129.8272426!3d32.7303522!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3515526e262ff8bf%3A0xbf9c354d7c1dfa1b!2z44CSODUwLTAwNzcg6ZW35bSO55yM6ZW35bSO5biC5bCP54Cs5oi455S677yY77yQ77yZ4oiS77yR77yS!5e0!3m2!1sja!2sjp!4v1783391180256!5m2!1sja!2sjp',
   },
   B: {
 	name: '株式会社メモリード 長崎仏光堂',
 	zip: '850-0046',
 	address: '長崎県長崎市幸町4-13',
 	tel: '095-825-6386',
-	hours: '9:00-17:00',
-	closed: '',
-	area: '',
+  　fax: '095-825-6395',   
+  　hours: '9:00-17:00',
+  　email: 'bukkoudou@memolead.co.jp',
+	closed: '--',
+	area: '--',
+    mapEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3355.3299010380197!2d129.8661348!3d32.756979799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3515532f8507bac1%3A0x12a38646aaaefa79!2z44CSODUwLTAwNDYg6ZW35bSO55yM6ZW35bSO5biC5bm455S677yU4oiS77yR77yT!5e0!3m2!1sja!2sjp!4v1783391238933!5m2!1sja!2sjp',
   },
 } as const;
 
